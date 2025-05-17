9c5ad3da9ae7e2f2bd84214afc153a1f17883eb6
Tafhim301 <tafhim301@gmail.com>
2025-05-17 07:26:47 +0600

    Fourth problem added

--- src/assignment.ts
+++ src/assignment.ts
@@ -13,22 +13,47 @@ function filterByRating(
 }
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
+  const output: T[] = [];
 
-    const output : T[] = [];
-
-    for (let array of arrays) {
-        for(let item of array){
-
-            output.push(item);
-
-            
-        }
-        
+  for (let array of arrays) {
+    for (let item of array) {
+      output.push(item);
     }
+  }
 
-    return output;
-  
-
-    
+  return output;
 }
 
+{
+  class Vehicle {
+    private make: string;
+    private year: number;
+
+    constructor(make: string, year: number) {
+      this.make = make;
+      this.year = year;
+    }
+
+    getInfo() {
+      return `Make: ${this.make}, Year: ${this.year}`;
+    }
+  }
+
+  class Car extends Vehicle {
+    private model: string
+    constructor(make: string, year : number, model: string ){
+      super(make,year);
+      
+      this.model = model;
+    }
+
+     getModel(){
+      return `Model: ${this.model}`
+    }
+  }
+
+  
+
+  
+
+}
